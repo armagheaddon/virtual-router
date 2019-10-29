ec69e7f8fcf3edc2fa62ab8e7fc44852fbb65338
u101040 <u101040@deshalit.local>
2019-10-29 12:50:41 +0200

    added stuff

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 try:
     import wmi
-except:
+except ImportError:
     import dependencies
     dependencies.install_wmi()
     import wmi
@@ -16,11 +16,17 @@ subnetmask = nic.IPSubnet[0]
 gateway = nic.DefaultIPGateway[0]
 
 # Current configurations
-print nic.DefaultIPGateway[0]
-print nic.IPAddress[0]
-print nic.IPSubnet[0]
+print gateway
+print ip
+print subnetmask
+# change to json
+open("previous_configurations", 'w').write(ip + " " + subnetmask + " " + gateway)
 
-# # Set IP address, subnetmask and default gateway
-# # Note: EnableStatic() and SetGateways() methods require *lists* of values to be passed
-# nic.EnableStatic(IPAddress=[ip],SubnetMask=[subnetmask])
-# nic.SetGateways(DefaultIPGateway=[gateway])
+# ip = 
+# subnetmask = 
+# gateway = 
+
+# Set IP address, subnetmask and default gateway
+# Note: EnableStatic() and SetGateways() methods require *lists* of values to be passed
+#nic.EnableStatic(IPAddress=[ip],SubnetMask=[subnetmask])
+nic.SetGateways(DefaultIPGateway=[gateway])
--- deconfigure.py
+++ deconfigure.py
@@ -7,4 +7,10 @@ nic_configs = wmi.WMI().Win32_NetworkAdapterConfiguration(IPEnabled=True)
 nic = nic_configs[0]
 
 # Enable DHCP
-nic.EnableDHCP()
+# nic.EnableDHCP()
+ip, subnetmask, gateway = open("previous_configurations", 'r').read().split(" ")
+
+# Set IP address, subnetmask and default gateway
+# Note: EnableStatic() and SetGateways() methods require *lists* of values to be passed
+#nic.EnableStatic(IPAddress=[ip],SubnetMask=[subnetmask])
+nic.SetGateways(DefaultIPGateway=[gateway])
